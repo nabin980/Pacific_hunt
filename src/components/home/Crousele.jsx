766c0b31b9ce01fc7be2efa31f37805b2493c384
Shenrt <olinaveen98@gmail.com>
2024-05-06 14:35:31 +0545

    res

--- src/components/home/Crousele.jsx
+++ src/components/home/Crousele.jsx
@@ -26,11 +26,11 @@ const Crousele = () => {
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 690 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 689, min: 0 },
       items: 1,
     },
   };
